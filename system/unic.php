2ccb0bd4060390c7857c32d770a29b7aa6292a96
Rajkumar Dusad <32831684+rajkumardusad@users.noreply.github.com>
2020-11-16 14:01:26 +0530

    Unic Framework

--- system/unic.php
+++ system/unic.php
@@ -6,7 +6,7 @@
 *
 * @package : Unic Framework
 * @author : Rajkumar Dusad
-* @copyright : Rajkumar Dusad
+* @copyright : Unic Framework
 * @license : MIT License
 * @link : https://github.com/unic-framework/unic
 */
@@ -23,25 +23,6 @@ include_once(SYSPATH.'/core/autoloader.php');
 //Include composer autoloader
 if(file_exists(BASEPATH.'/vendor/autoload.php')) {
   include_once(BASEPATH.'/vendor/autoload.php');
-}
-
-//Set unic error handler
-set_error_handler('handleError');
-
-/**
-* Debug setting
-* Set the error_reporting directive at runtime.
-*/
-if($debug === FALSE) {
-  //turn off all error reporting.
-  error_reporting(0);
-  if(function_exists('ini_set')) {
-    ini_set('display_errors', 0);
-  }
-} else {
-  //turn on all error reporting.
-  //It will display E_ERROR, E_WARNING, and E_PARSE error, it will not display any E_NOTICE and other error. to display all errors use E_ALL or -1 in error_reporting.
-  error_reporting(E_ERROR | E_WARNING | E_PARSE);
 }
 
 /*
