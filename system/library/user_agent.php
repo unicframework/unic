6cb6ad95da83d7c2d1034c602a7c24099cc49103
Rajkumar Dusad <32831684+rajkumardusad@users.noreply.github.com>
2020-12-09 14:05:36 +0530

    Unic Framework

--- system/library/user_agent.php
+++ system/library/user_agent.php
@@ -6,7 +6,7 @@
 * @package : User Agent Library
 * @category : Library
 * @author : Unic Framework
-* @link : https://github.com/unic-framework/unic
+* @link : https://github.com/unicframework/unic
 */
 
 defined('SYSPATH') OR exit('No direct access allowed');
