5823f484e0813789d695fff82b62895f766d2f85
Rajkumar Dusad <32831684+rajkumardusad@users.noreply.github.com>
2021-01-23 07:22:08 +0530

    Unic Framework

---
+++ system/core/Middleware.php
@@ -0,0 +1,14 @@
+<?php
+/**
+* Middleware
+* Handle middlewares.
+*
+* @package : Middleware
+* @category : System
+* @author : Unic Framework
+* @link : https://github.com/unicframework/unic
+*/
+
+class Middlewares extends Response {
+  //Extends response object
+}
