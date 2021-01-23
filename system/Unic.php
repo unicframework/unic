e7f54621563b08a9025878f0148235c661a93388
Rajkumar Dusad <32831684+rajkumardusad@users.noreply.github.com>
2021-01-23 07:30:38 +0530

    Unic Framework

--- system/Unic.php
+++ system/Unic.php
@@ -18,7 +18,7 @@ define('SYSPATH', rtrim(__DIR__, '/'));
 define('BASEPATH', rtrim(dirname(__DIR__), '/'));
 
 //Include system autoloader
-require_once SYSPATH.'/core/autoloader.php';
+require_once SYSPATH.'/core/Autoloader.php';
 
 //Include composer autoloader
 if(file_exists(BASEPATH.'/vendor/autoload.php')) {
