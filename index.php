06eb1dcf9f15a8ecf12a62653f14f4cc4cc43e69
Rajkumar Dusad <32831684+rajkumardusad@users.noreply.github.com>
2022-12-18 11:43:49 +0530

    Update index.php

--- index.php
+++ index.php
@@ -13,4 +13,9 @@ $app->get('/api', function($req, $res) {
   $res->json(['status' => 'Ok']);
 });
 
+// Error handler middleware
+$app->use(function($err, $req, $res, $next) {
+  $res->send('Internal Server Error', 500);
+});
+
 $app->start();
