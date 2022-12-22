f88bd5c8182d64bff9cc4e07cde29751716ab384
Rajkumar Dusad <32831684+rajkumardusad@users.noreply.github.com>
2022-12-22 22:39:40 +0530

    Update index.php

--- index.php
+++ index.php
@@ -11,17 +11,30 @@ $app->static('/', base_path('public'));
 // Set view path
 $app->set('views', base_path('views'));
 
+// Set view engine
+$app->set('view_engine', 'twig');
+$app->set('cache_path', base_path('.cache'));
+
 $app->get('/', function($req, $res) {
-  $res->render('index.html');
+    $res->render('index.twig', [
+        'title' => 'Unic',
+    ]);
 });
 
 $app->get('/api', function($req, $res) {
-  $res->json(['status' => 'Ok']);
+    $res->json([
+        'status' => 'Ok',
+    ]);
 });
 
 // Error handler middleware
 $app->use(function($err, $req, $res, $next) {
-  $res->send('Internal Server Error', 500);
+    $res->status(500)
+        ->render('error.twig', [
+            'message' => $err->getMessage(),
+            'stack' => $err->getTraceAsString(),
+            'status' => 500,
+        ]);
 });
 
 $app->start();
