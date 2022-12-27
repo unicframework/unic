4f0466dea0bb57697aec440d83e224ad22e7ef6e
Rajkumar Dusad <32831684+rajkumardusad@users.noreply.github.com>
2022-12-27 14:34:03 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -6,29 +6,31 @@ use Unic\App;
 $app = new App();
 
 // Set public path
-$app->static('/', base_path('public'));
+$app->use($app->static('/', base_path('public')));
 
 // Set view path
 $app->set('views', base_path('views'));
 
 // Set view engine
-$app->set('view_engine', 'twig');
-$app->set('cache_path', base_path('.cache'));
+$app->set('view_engine', 'twig', [
+    'cache' => base_path('.cache')
+]);
 
-$app->get('/', function($req, $res) {
+// Handle routes
+$app->get('/', function ($req, $res) {
     $res->render('index.twig', [
         'title' => 'Unic',
     ]);
 });
 
-$app->get('/api', function($req, $res) {
+$app->get('/api', function ($req, $res) {
     $res->json([
         'status' => 'Ok',
     ]);
 });
 
 // Error handler middleware
-$app->use(function($err, $req, $res, $next) {
+$app->use(function ($err, $req, $res, $next) {
     $res->status(500)
         ->render('error.twig', [
             'message' => $err->getMessage(),
